b2dae11e5bc391fa2c189de3f05b6cdca4965bfb
Leinnan <siuszko@zoho.com>
2025-12-02 20:04:47 +0000

    Build: (3964410) Fix mockup, add time to logs

---
+++ mockup.d.ts
@@ -0,0 +1,44 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: (a: number, b: number) => number;
+  readonly wasm_bindgen__convert__closures_____invoke__h6dde22b3c07cd8fb: (a: number, b: number, c: any, d: any) => void;
+  readonly wasm_bindgen__closure__destroy__h15e7a720e36074cb: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures_____invoke__h718d214720108357: (a: number, b: number, c: any) => void;
+  readonly wasm_bindgen__convert__closures_____invoke__h90cce68c190d651b: (a: number, b: number, c: any) => void;
+  readonly wasm_bindgen__closure__destroy__h6e6bee901e62538a: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures_____invoke__hc7dda81e49a7ce10: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_externrefs: WebAssembly.Table;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
