c101542f338cfa317cd8ed28df3d200fc10c257a
Leinnan <siuszko@zoho.com>
2026-03-18 08:22:24 +0000

    Build: (78e011e) 0.3 version

---
+++ mockup_bg.wasm.d.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const wasm_bindgen__closure__destroy__h5d9a08338d22fa76: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h09968e00c431b7f8: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h86b7228491fd2912: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hdf2267373674c629: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h9b4bf2576183a7f2: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h244d342a6e1bb74d_9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h65e3c9c77b6e0eee: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hfe9fa0861a29e559: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1d50643b25f3673d: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
